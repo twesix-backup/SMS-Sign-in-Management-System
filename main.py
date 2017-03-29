d426ab1d4376f17f2679e74898e4f4721dc286ee
孟政元 <278227739@qq.com>
2017-03-29 19:15:41 +0800

    finish query-by-class

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ conn = sqlite3.connect('dbs/sms.db')
 def run_sql(sql):
     try:
         cursor = conn.cursor()
-        conn.execute(sql)
+        cursor.execute(sql)
         conn.commit()
+        return cursor
     except sqlite3.OperationalError as e:
-        print('[x]出现错误，签到失败')
+        print('[x]出现错误')
         print(e)
 
 
@@ -23,11 +24,15 @@ def hint():
     print('    3.[按班查询]  sign-by-class 班级号 \n')
 
 
-def query_by_class(cid):
+def query_by_class(cid, date):
+    sql = "select name,sid from sign where sid like '{cid}%' and date='{date}' ;".format(cid=cid, date=date)
+    cursor = run_sql(sql)
+    for line in cursor.fetchall():
+        print(line)
     pass
 
 
-def query_by_student(sid):
+def query_by_student(sid, date):
     pass
 
 
@@ -59,18 +64,18 @@ while True:
 
     if command[0] == 'query-by-student':
         print('[按学号查询]')
-        if len(command) != 2:
+        if len(command) != 3:
             print('参数个数不对')
             continue
-        query_by_student(command[1])
+        query_by_student(command[1], command[2])
         continue
 
     if command[0] == 'query-by-class':
         print('[按班级查询]')
-        if len(command) != 2:
+        if len(command) != 3:
             print('参数个数不对')
             continue
-        query_by_class(command[1])
+        query_by_class(command[1], command[2])
         continue
 
     print('[x]命令错误,请重新输入')
