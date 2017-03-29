3d5a6a3ea4bd519c5422283e53130606b10d0763
孟政元 <278227739@qq.com>
2017-03-29 18:09:55 +0800

    finish sign-in,optimize hints

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import sqlite3
+from datetime import datetime
 
 conn = sqlite3.connect('dbs/sms.db')
 
 
 def run_sql(sql):
-    cursor = conn.cursor()
-    conn.execute(sql)
-    conn.commit()
+    try:
+        cursor = conn.cursor()
+        conn.execute(sql)
+        conn.commit()
+    except sqlite3.OperationalError as e:
+        print('[x]出现错误，签到失败')
+        print(e)
 
 
 def hint():
+    print('//--------------------------------------------//')
     print('请按下列格式使用 : ')
-    print('    1.[签到]     sign-in 姓名 班级')
+    print('[注意] 学号格式为4位班级号加4位学生编号，共八位（例如：53140821）')
+    print('    1.[签到]      sign-in 学号')
     print('    2.[按人查询]  query-by-student 学号')
     print('    3.[按班查询]  sign-by-class 班级号 \n')
 
@@ -24,14 +31,17 @@ def query_by_student(sid):
     pass
 
 
-def sign_in(name, cid):
+def sign_in(name, sid):
+    now = str(datetime.now()).split(' ')[0]
+    sql = "insert into sign (name,sid,date) values('{name}','{sid}','{now}') ;".format(name=name, sid=sid, now=now)
+    run_sql(sql)
     pass
 
 
 command = 'start'
-hint()
 
 while True:
+    hint()
     command = input()
     command = command.split(' ')
 
@@ -40,15 +50,27 @@ while True:
         break
 
     if command[0] == 'sign-in':
-        print('sign-in')
+        print('[签到]')
+        if len(command) != 3:
+            print('参数个数不对')
+            continue
+        sign_in(command[1], command[2])
         continue
 
     if command[0] == 'query-by-student':
-        print('query-by-student')
+        print('[按学号查询]')
+        if len(command) != 2:
+            print('参数个数不对')
+            continue
+        query_by_student(command[1])
         continue
 
     if command[0] == 'query-by-class':
-        print('query-by-class')
+        print('[按班级查询]')
+        if len(command) != 2:
+            print('参数个数不对')
+            continue
+        query_by_class(command[1])
         continue
 
     print('[x]命令错误,请重新输入')
